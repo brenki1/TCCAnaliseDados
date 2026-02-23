62f8ba7339bf232b543a0dc61bf7f8607c834a4b
Osvaldo <pagioliosvaldo@gmail.com>
2026-02-23 17:15:52 -0300

    renomeando variaveis

--- Kyoto.R
+++ Kyoto.R
@@ -10,6 +10,20 @@ kyoto01012015 <- read_delim(
   show_col_types = FALSE
 )
 
-sum(kyoto01012015)
 
-rename(kyoto01012015, )
+kyoto01012015 <- kyoto01012015 %>% rename( 
+       Duracao = X1,
+       Servico = X2,
+       Bytes_fonte = X3,
+       Bytes_destino = X4,
+       Qtd = X5,
+       Tx_msm_servico = X6,
+       Tx_Serro = X7,
+       Tx_Serro_servico = X8,
+       Destino_qtd_host = X9,
+       Destino_host_qtd_servico = X10)
+
+
+
+
+
